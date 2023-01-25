c4908a23cc0b72a81db738ff81e5adaee1545a1d
Manny-OnGit <manny.fluss@gmail.com>
2023-01-24 23:25:23 -0800

    start of implementing interchangeable scripts

--- Assets/Scripts/BaseAbilityScript.cs
+++ Assets/Scripts/BaseAbilityScript.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseAbilityScript : MonoBehaviour
+
+//this is 
+public  class BaseAbilityScript : MonoBehaviour
 {
+    GameObject baseClass;
+    
     // Start is called before the first frame update
-    void Start()
+    public virtual void OnEquip()
     {
-        
+        Debug.Log("equipped");
+    }
+    public virtual void OnDrop()
+    {
+        Debug.Log("dropped");
+    }
+    public virtual void OnButtonClick()
+    {
+        Debug.Log("button clicked");
+
+    }
+    public virtual void OnButtonHeldDown()
+    {
+        Debug.Log("button held down");
+
+    }
+    public virtual void OnButtonReleased()
+    {
+        Debug.Log("button released");
     }
 
-    // Update is called once per frame
-    void Update()
+}
+
+public class testAbility : BaseAbilityScript
+{
+    
+    public override void OnEquip()
     {
+        base.OnEquip();
+        Debug.Log("I am mr test fear me");
         
     }
 }
+
--- Assets/Scripts/CharacterAbilityScript.cs
+++ Assets/Scripts/CharacterAbilityScript.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class CharacterAbilityScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //contains references to the currently chosen abilities
+    List<BaseAbilityScript> playerAbilities = new List<BaseAbilityScript> {null,null,null};
+
+
     void Start()
     {
-        
+        equipAbility("testAbility",0);
+        removeAbility(0);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void equipAbility(string _toInsert, int _index)
     {
+        if (_index >= playerAbilities.Count)
+        {
+            return;
+        }
+        if (playerAbilities[_index]!=null)
+        {
+            removeAbility(_index);
+        }
+        //this will have to be modified whenever a new ability script is made
+        switch(_toInsert)
+        {
+            case "testAbility":
+                playerAbilities[_index] = gameObject.AddComponent<testAbility>();
+                break;
+            default:
+                break;
+        }
         
+        playerAbilities[_index].OnEquip();
+
+
     }
+    void removeAbility(int _index)
+    {
+
+        if (_index >= playerAbilities.Count)
+        {
+            return;
+        }
+        if (playerAbilities[_index]!=null)
+        {   
+
+                   
+            //fire signal on item and remove from array
+            playerAbilities[_index].OnDrop();
+            Destroy(playerAbilities[_index]);  
+            playerAbilities[_index] = null;
+        }
+    }
+
+
 }
