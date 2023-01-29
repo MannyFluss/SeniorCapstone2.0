91e8780161b3eaca26be2a0be201f2fe03dd8961
Yuhao <hello@chenyuhao.me>
2023-01-28 17:13:34 -0800

    Dungeon Init

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour
+{
+    public GameObject playerSpawn;
+    public GameObject playerExit;
+    public GameObject[] minionSpawns = new GameObject[0];
+}
---
+++ Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    List<Dungeon> dungeons = new List<Dungeon> { null };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
