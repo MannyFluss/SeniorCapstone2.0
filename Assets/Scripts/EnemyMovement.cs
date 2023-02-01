caa2214c247b4a61851fc5082415db4a9edbfb9c
Yuhao <hello@chenyuhao.me>
2023-01-31 16:26:05 -0800

    +fixed assigned correct prefab for enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -11,6 +11,12 @@ public class EnemyMovement : MonoBehaviour
     //for movement
     private Transform player;
 
+    // for movement optimization
+    private bool _isInContact;
+
+    // for groudcheck
+    private Rigidbody rb;
+
     //changeable vars
     [Header("Changeable Values")]
     [SerializeField]
@@ -19,24 +25,66 @@ public class EnemyMovement : MonoBehaviour
     private float moveSpeed = 0.01f;
 
 
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed);
-        if(_hit)
+        if (_hit)
         {
-            transform.position = new Vector3(Vector3.MoveTowards(transform.position, moveFrom.position, -knockback).x, transform.position.y, Vector3.MoveTowards(transform.position, moveFrom.position, -knockback).z);
+            KnockBack(moveFrom: moveFrom.position);
+        }
+
+        if (IsGrounded())
+        {
+            if (!_isInContact)
+            {
+                MoveToPlayer();
+                _isInContact = false;
+            }
         }
     }
+
+    private bool IsGrounded() {
+        float _distanceToTheGround = GetComponent<Collider>().bounds.extents.y;
+        return Physics.Raycast(transform.position, Vector3.down, _distanceToTheGround + 0.1f);
+    }
+
+    private void MoveToPlayer()
+    {
+        Vector3 moveTo = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, moveSpeed);
+    }
+
+    private void KnockBack(Vector3 moveFrom)
+    {
+        transform.position = new Vector3(Vector3.MoveTowards(transform.position, moveFrom, -knockback).x, transform.position.y, Vector3.MoveTowards(transform.position, moveFrom, -knockback).z);
+    }
+  
     private void OnTriggerEnter(Collider other)
     {
+        
+
         if(other.gameObject.tag == "Attack")
         {
             moveFrom = other.transform.parent.parent;
             StartCoroutine(hit());
+        }
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player") {
+            _isInContact = true;
+        }
+
+        if(other.tag == "Enemy")
+        {
+            _isInContact = true;
         }
     }
 
