64c4ecdbf422b4b7b5f5d453754c1f7bd2926d7e
Manny-OnGit <manny.fluss@gmail.com>
2023-02-14 19:09:30 -0800

    UI shenanigans started

--- Assets/Scripts/ShopKeeperScript.cs
+++ Assets/Scripts/ShopKeeperScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DialogueEditor;
 
 public class ShopKeeperScript : MonoBehaviour
 {
-
+    public NPCConversation myConversation;
     float interactRange = 5.0f;
+    bool inRange = false;
     GameObject player;
     CharacterAbilityScript playerAbilityManager;
 
@@ -19,8 +21,9 @@ public class ShopKeeperScript : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        shopUI.enabled = false;
+        //shopUI.enabled = false;
         playerAbilityManager = player.GetComponent<CharacterAbilityScript>();
+        ConversationManager.Instance.StartConversation(myConversation);
     }
 
     // Update is called once per frame
@@ -29,13 +32,23 @@ public class ShopKeeperScript : MonoBehaviour
         playMoneyText.text = "Player Money: " + playerAbilityManager.playerMoney;
         
         var di = Vector3.Distance(transform.position,player.transform.position);
-        if (di <= interactRange)
+        if (di <= interactRange && inRange == false)
         {
-            shopUI.enabled = true;
-        }
-        else
+            inRange = true;
+            Debug.Log(myConversation);
+
+            
+        }else if (di <= interactRange)
         {
-            shopUI.enabled = false;
+            inRange = true;
+        }else if (di > interactRange)
+        {
+            inRange = false;
         }
+    }
+
+    public void initiateShop()
+    {
+        Debug.Log("this is where the shop will start doing shop things");
     }
 }
