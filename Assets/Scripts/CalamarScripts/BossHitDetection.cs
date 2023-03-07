41277fc41ba32eb0d17b5693c1474e00891300dc
dylhigashi <dhigashi@ucsc.edu>
2023-03-06 22:51:23 -0800

    +BossUI

--- Assets/Scripts/CalamarScripts/BossHitDetection.cs
+++ Assets/Scripts/CalamarScripts/BossHitDetection.cs
@@ -16,7 +16,7 @@ public class BossHitDetection : MonoBehaviour
     {
         if(other.gameObject.tag == "Attack" && bb.canBeHit)
         {
-            bb.health-= 3;
+            bb.health-= 0.8f;
         }
     }
 }
