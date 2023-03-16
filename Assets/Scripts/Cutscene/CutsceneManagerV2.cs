4064ee77d84fe9cbff3c53ff12b0bf317fca8f5c
Kai Xiang (Neo) Zhang <neo.cyc@gmail.com>
2023-03-16 15:56:36 -0700

    Quick Push

--- Assets/Scripts/Cutscene/CutsceneManagerV2.cs
+++ Assets/Scripts/Cutscene/CutsceneManagerV2.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class CutsceneManagerV2 : MonoBehaviour
 {
-    private Dictionary<Image, string> cutscene = new Dictionary<Image, string>();
+    //private Dictionary<Image, string> cutscene = new Dictionary<Image, string>();
+    public List<Dictionary<Image, string>> cutscene = new List<Dictionary<Image, string>>();
 
-    public List<cutscene>
     // Start is called before the first frame update
     void Start()
     {
