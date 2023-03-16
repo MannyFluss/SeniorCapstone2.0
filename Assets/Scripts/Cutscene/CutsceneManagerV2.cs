2a818f023c75a5d62c1c0f745abaf8579bb72e44
Neo <neo.cyc@gmail.com>
2023-03-16 15:05:59 -0700

    Building Cutscene

---
+++ Assets/Scripts/Cutscene/CutsceneManagerV2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneManagerV2 : MonoBehaviour
+{
+    private Dictionary<Image, string> cutscene = new Dictionary<Image, string>();
+
+    public List<cutscene>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
