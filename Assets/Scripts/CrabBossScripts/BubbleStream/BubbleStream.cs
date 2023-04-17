7f0d411997989ffab205b1bc82ae7bb3995f4363
dylhigashi <dhigashi@ucsc.edu>
2023-04-17 12:33:47 -0700

    +monday changes

---
+++ Assets/Scripts/CrabBossScripts/BubbleStream/BubbleStream.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleStream : MonoBehaviour
+{
+    BubbleStreamCannon[] cannons = new BubbleStreamCannon[6];
+    //GameObject[] bubbleBullet = new GameObject[5];
+
+    [SerializeField]
+    public GameObject indicator;
+
+    void Start()
+    {
+       for(int i = 0; i < transform.childCount; i ++)
+        {
+            cannons[i] = transform.GetChild(i).GetComponent<BubbleStreamCannon>();
+        }
+        bubbleStream(100);
+    }
+
+    
+
+    public void bubbleStream(int health)
+    {
+        //random values
+        int[] arr = { 1, 2, 3, 4, 5, 0 };
+        for (int t = 0; t < arr.Length; t++)
+        {
+            int tmp = arr[t];
+            int r = Random.Range(t, arr.Length);
+            arr[t] = arr[r];
+            arr[r] = tmp;
+        }
+
+        
+
+        if (health >= 60)
+        {
+            cannons[arr[0]].fireSingleCannon();
+            cannons[arr[1]].fireSingleCannon();
+        }
+        else if(health < 60 && health >= 30)
+        {
+            cannons[arr[0]].fireSingleCannon();
+            cannons[arr[1]].fireSingleCannon();
+            cannons[arr[2]].fireSingleCannon();
+        }
+        else
+        {
+            cannons[arr[0]].fireSingleCannon();
+            cannons[arr[1]].fireSingleCannon();
+            cannons[arr[2]].fireSingleCannon();
+            cannons[arr[3]].fireSingleCannon();
+        }
+    }
+
+}
