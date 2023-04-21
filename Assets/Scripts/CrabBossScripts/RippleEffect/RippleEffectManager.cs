b744d47683800f98e612e2fc986a5d9ad40d71ea
Neo <neo.cyc@gmail.com>
2023-04-20 18:49:32 -0700

    Quick Change to Interval in Ripple

--- Assets/Scripts/CrabBossScripts/RippleEffect/RippleEffectManager.cs
+++ Assets/Scripts/CrabBossScripts/RippleEffect/RippleEffectManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RippleEffectManager : MonoBehaviour
 {
+    private float interval = 0.0f;
+
     RippleEffectSingle[] ripples = new RippleEffectSingle[4];
     void Start()
     {
@@ -20,19 +22,19 @@ public class RippleEffectManager : MonoBehaviour
 
     IEnumerator rippleEffectAttack(int health)
     {
-        float interval = 0;
+        // float interval = 0;
         var num = 0;
 
         //determine interval and number of waves
         if (health > 50)
         {
             num = Random.Range(7, 11);
-            interval = 0.5f;
+            interval = 1.5f;
         }
         else
         {
             num = Random.Range(8, 12);
-            interval = 0.3f;
+            interval = 1.0f;
         }
         for(int i = 0; i < num; i++)
         {
