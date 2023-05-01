3cf176a6d8798e29dccab88536995b1a391460ae
Danny-Chung09 <dchung6969@gmail.com>
2023-05-01 01:24:18 -0700

    crane animation and hitbox fix

--- Assets/Scripts/TutorialRoom2.cs
+++ Assets/Scripts/TutorialRoom2.cs
@@ -16,18 +16,22 @@ public class TutorialRoom2 : MonoBehaviour
         dm = GetComponentInParent<DungeonManager>();
         player = GameObject.FindGameObjectWithTag("Player");
         clearR2 = false;
+        //armScript.Frenzy = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 playerPos = player.transform.position;
+        //checks if player is in room 2 
         if (playerPos.x >= 65 && playerPos.x <= 95 && !clearR2) {
+            //checks if room is cleared
             if (gameObject.GetComponentInParent<Dungeon>().RemainMinions() == 0) {
                 clearR2 = true;
                 gameObject.SetActive(false);
                 return;
             }
+            //checks if room has enemies
             foreach (GameObject enemies in GameObject.FindGameObjectsWithTag("Enemy"))
             {
                 var distanceToEnemy = Vector3.Distance(gameObject.transform.position,enemies.transform.position);
@@ -37,8 +41,6 @@ public class TutorialRoom2 : MonoBehaviour
                     return;
                 } 
             }
-        } else {
-            armScript.Frenzy = false;
         }
     }
 }
