93843115a0554a6aa280d8bb569d0cb982c582c8
Neo <neo.cyc@gmail.com>
2023-05-08 22:48:54 -0700

    Minor Changes

--- Assets/Scripts/LoadHackNSlash.cs
+++ Assets/Scripts/LoadHackNSlash.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadHackNSlash : MonoBehaviour
 {
+    public string SceneName;
     // Start is called before the first frame update
     public void OnTriggerEnter(Collider other) {
         // Load the new scene.
-        SceneManager.LoadScene("HackNSlash");
+        SceneManager.LoadScene(SceneName);
     }
 }
